c96ee3afb84047bf6bcb1d7cea83038b4472173e
zhangzixing <3273448110@qq.com>
2021-02-06 17:16:03 +0800

    修订版2.0

--- src/main/java/com/zzh/mybatisplusdaynamicannotationsql/config/ymlConfigurationProperties.java
+++ src/main/java/com/zzh/mybatisplusdaynamicannotationsql/config/ymlConfigurationProperties.java
@@ -1,9 +1,13 @@
 package com.zzh.mybatisplusdaynamicannotationsql.config;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.convert.DataSizeUnit;
 import org.springframework.boot.convert.DurationUnit;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
@@ -19,8 +23,12 @@ import java.util.Map;
  * ignoreInvalidFields=true: 忽略绑定失败的值，只是为null
  * ignoreUnknownFields = false: yml配置文件中的字段，ymlConfigurationProperties中并没有配置，启动时会报错，提示此属性还未绑定
  */
+@Slf4j
 @Component
 @Data
+//yml或者properties文件中存在 key：enAble value：enAble的键值对，此配置文件才会生效
+@ConditionalOnProperty(name = "enAble", havingValue = "enAble")
+//绑定前缀为a的属性，忽略绑定失败的属性
 @ConfigurationProperties(prefix = "a", ignoreInvalidFields = true)
 public class ymlConfigurationProperties {
     private String name = "cName";
@@ -46,4 +54,13 @@ public class ymlConfigurationProperties {
      * 绑定map类型
      */
     Map<String, String> map;
+
+    /**
+     * ioc容器中没有此bean（testBean），onMissTestBean才会注入。
+     */
+    @Bean
+    @ConditionalOnMissingBean(testBean.class)
+    public void onMissTestBean() {
+        System.out.println("ConditionalOnMissingBean注解生效：");
+    }
 }
